1db26161475eb7f98637bc5c18009dc2f5277a92
boniface mwandobo <bonifacesimon04@gmail.com>
2026-08-19 13:09:54 +0300

    add unit to employee

--- src/main/java/com/mwalimubank/mbimsapi/features/administration/employee/entity/EmployeeEntity.java
+++ src/main/java/com/mwalimubank/mbimsapi/features/administration/employee/entity/EmployeeEntity.java
@@ -1,6 +1,8 @@
 package com.mwalimubank.mbimsapi.features.administration.employee.entity;
 
 import com.mwalimubank.mbimsapi.core.entity.BaseEntity;
+import com.mwalimubank.mbimsapi.features.administration.unit.UnitEntity;
+import com.mwalimubank.mbimsapi.features.user.UserEntity;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -32,5 +34,9 @@ public class EmployeeEntity extends BaseEntity {
 
     @Column()
     private String mobilePhone;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "unit_id")
+    private UnitEntity unit;
 
 }
